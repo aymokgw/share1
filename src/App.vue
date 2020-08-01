dd8077dd1bd4e0911bc2e3af35570e2bfcd8fe5f
aymokgw <aymokgw0823@gmail.com>
2020-08-01 18:02:17 +0900

    sharefirst

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div id="app">
+   <router-view />
+ </div>
+</template>
+
+<style>
+html {
+ background-color: #15202b;
+}
+* {
+ margin: 0;
+ padding: 0;
+ border: 0;
+ color: white;
+ font-family: "Noto Sans JP";
+}
+</style>
